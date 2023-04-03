0e912d0b32a1f3718006233de33f310f0c3d0e72
kannakalyan05 <kannakalyan05@gmail.com>
2023-04-03 18:48:49 +0530

    done

--- rofi/applets/bin/screenshot.sh
+++ rofi/applets/bin/screenshot.sh
@@ -74,18 +74,6 @@ if [[ ! -d "$dir" ]]; then
 	mkdir -p "$dir"
 fi
 
-# notify and view screenshot
-notify_view() {
-	notify_cmd_shot='dunstify -u low --replace=699'
-	${notify_cmd_shot} "Copied to clipboard."
-	viewnior ${dir}/"$file"
-	if [[ -e "$dir/$file" ]]; then
-		${notify_cmd_shot} "Screenshot Saved."
-	else
-		${notify_cmd_shot} "Screenshot Deleted."
-	fi
-}
-
 # Copy screenshot to clipboard
 copy_shot () {
 	tee "$file" | xclip -selection clipboard -t image/png
@@ -102,29 +90,24 @@ countdown () {
 # take shots
 shotnow () {
 	cd ${dir} && sleep 0.5 && maim -u -f png | copy_shot
-	notify_view
 }
 
 shot5 () {
 	countdown '5'
 	sleep 1 && cd ${dir} && maim -u -f png | copy_shot
-	notify_view
 }
 
 shot10 () {
 	countdown '10'
 	sleep 1 && cd ${dir} && maim -u -f png | copy_shot
-	notify_view
 }
 
 shotwin () {
 	cd ${dir} && maim -u -f png -i `xdotool getactivewindow` | copy_shot
-	notify_view
 }
 
 shotarea () {
 	cd ${dir} && maim -u -f png -s -b 2 -c 0.35,0.55,0.85,0.25 -l | copy_shot
-	notify_view
 }
 
 # Execute Command
@@ -161,5 +144,6 @@ case ${chosen} in
 		run_cmd --opt5
         ;;
 esac
+
 
 
