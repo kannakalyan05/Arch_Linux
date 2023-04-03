568e5385d1f32fcdadc3d01ebd3e5c48d2c75821
kannakalyan05 <kannakalyan05@gmail.com>
2023-04-03 20:20:20 +0530

    refined

--- rofi/powermenu/type-2/powermenu.sh
+++ rofi/powermenu/type-2/powermenu.sh
@@ -12,7 +12,7 @@
 
 # Current Theme
 dir="$HOME/.config/rofi/powermenu/type-2"
-theme='style-2'
+theme='style-3'
 
 # CMDs
 uptime="`uptime -p | sed -e 's/up //g'`"
@@ -24,8 +24,8 @@ reboot=''
 lock=''
 suspend=''
 logout=''
-yes=''
-no=''
+#yes=''
+#no=''
 
 # Rofi CMD
 rofi_cmd() {
@@ -36,22 +36,22 @@ rofi_cmd() {
 }
 
 # Confirmation CMD
-confirm_cmd() {
-	rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 350px;}' \
-		-theme-str 'mainbox {children: [ "message", "listview" ];}' \
-		-theme-str 'listview {columns: 2; lines: 1;}' \
-		-theme-str 'element-text {horizontal-align: 0.5;}' \
-		-theme-str 'textbox {horizontal-align: 0.5;}' \
-		-dmenu \
-		-p 'Confirmation' \
-		-mesg 'Are you Sure?' \
-		-theme ${dir}/${theme}.rasi
-}
+#confirm_cmd() {
+#	rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 350px;}' \
+#		-theme-str 'mainbox {children: [ "message", "listview" ];}' \
+#		-theme-str 'listview {columns: 2; lines: 1;}' \
+#		-theme-str 'element-text {horizontal-align: 0.5;}' \
+#		-theme-str 'textbox {horizontal-align: 0.5;}' \
+#		-dmenu \
+#		-p 'Confirmation' \
+#		-mesg 'Are you Sure?' \
+#		-theme ${dir}/${theme}.rasi
+#}
 
 # Ask for confirmation
-confirm_exit() {
-	echo -e "$yes\n$no" | confirm_cmd
-}
+#confirm_exit() {
+#	echo -e "$yes\n$no" | confirm_cmd
+#}
 
 # Pass variables to rofi dmenu
 run_rofi() {
@@ -60,8 +60,8 @@ run_rofi() {
 
 # Execute Command
 run_cmd() {
-	selected="$(confirm_exit)"
-	if [[ "$selected" == "$yes" ]]; then
+	#selected="$(confirm_exit)"
+	#if [[ "$selected" == "$yes" ]]; then
 		if [[ $1 == '--shutdown' ]]; then
 			systemctl poweroff
 		elif [[ $1 == '--reboot' ]]; then
@@ -80,7 +80,7 @@ run_cmd() {
 			elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
 				qdbus org.kde.ksmserver /KSMServer logout 0 0 0
 			fi
-		fi
+		#fi
 	else
 		exit 0
 	fi
